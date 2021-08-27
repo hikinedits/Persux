b5ba0a6e5dbbe4a3a0fd7f9316b8ebf072adf160
Lursy <70035797+Lursy@users.noreply.github.com>
2021-08-27 12:32:52 -0300

    Update install.py

--- data/install.py
+++ data/install.py
@@ -4,16 +4,16 @@ from data.ferramentas import *
 def install():
     global nms
     os.chdir('/data/data/com.termux/files/home/')
-    Ver = os.path.exists('user')
+    Ver = os.path.exists('.usuario')
     if not Ver:
         os.system('clear')
-        user = str(input(f'{vd}Nick: '))
+        user = str(input(f'{vd}Nick: {ve}'))
         os.system('clear')
         os.system('pkg install figlet -y')
         os.chdir('/data/data/com.termux/files/usr/etc/')
         os.system('rm -rf motd')
         os.chdir('/data/data/com.termux/files/home/')
-        user_file = open('user', 'w')
+        user_file = open('.usuario', 'w')
         user_file.write(user)
         user_file.close()
         os.system('clear')
