988013b17a4d707574096c0c8ef34edaa18fa0c7
Lursy <70035797+Lursy@users.noreply.github.com>
2021-08-27 23:29:45 -0300

    Update install.py

--- data/install.py
+++ data/install.py
@@ -1,4 +1,5 @@
-from data.ferramentas import *
+from data.ferramentas import vd, ve
+import os
 
 
 def install():
@@ -7,13 +8,13 @@ def install():
     Ver = os.path.exists('.usuario')
     if not Ver:
         os.system('clear')
-        user = str(input(f'{vd}Nick: {ve}'))
+        user = str(input(f'{vd}Nome: {ve}'))
         os.system('clear')
         os.system('pkg install figlet -y')
         os.chdir('/data/data/com.termux/files/usr/etc/')
         os.system('rm -rf .Cor')
         cor = open('.Cor', 'w')
-        cor.write(f'{cor_letra.replace(":cor", "37")}')
+        cor.write(f"PS1='\[\e[0;32m\]\w\[\e[0m\] \[\e[0;97m\]\$\[\e[0m\] '")
         cor.close()
         os.system('rm -rf motd')
         os.chdir('/data/data/com.termux/files/home/')
