df7e4d57743caa0f7c47129df26a0c5b2f20eb17
Lursy <70035797+Lursy@users.noreply.github.com>
2021-08-28 10:37:57 -0300

    Update banner.py

--- data/banner.py
+++ data/banner.py
@@ -3,40 +3,39 @@ from data.menu import bannerf, menu_cores
 
 
 def banner():
-    erro = 0
-    snick = rsenha = ''
-    os.system('clear')
-    print(bannerf)
-    nick = str(input(f'{am}Nick: {br}'))
-    nick_banner = banner_nick.replace(':name', f'{nick}')
-    rcor = open('.Cor', 'r')
-    vs = os.path.exists('.Senha')
-    if vs:
-        rsenha = open('.Senha', 'r')
-    print(menu_cores)
-    name = str(input(f'\n{am}//: {br}'))
-    os.chdir('/data/data/com.termux/files/usr/etc')
-    os.system('rm -rf bash.bashrc')
-    if name == '1':
-        snick = nick_banner.replace(':cor', '31')
-    elif name == '2':
-        snick = nick_banner.replace(':cor', '33')
-    elif name == '3':
-        snick = nick_banner.replace(':cor', '37')
-    elif name == '4':
-        snick = nick_banner.replace(':cor', '32')
-    elif name == '5':
-        snick = nick_banner.replace(':cor', '36')
-    elif name == '6':
-        snick = nick_banner.replace(':cor', '34')
-    elif name == '7':
-        snick = nick_banner.replace(':cor', '35')
-    else:
-        print(f'{ve}{cores} não é um comando')
-        sleep(2)
+    while True:
+        rsenha = ''
         os.system('clear')
-        erro = 1
-    if erro == 0:
+        print(bannerf)
+        nick = str(input(f'{am}Nick: {br}'))
+        nick_banner = banner_nick.replace(':name', f'{nick}')
+        rcor = open('.Cor', 'r')
+        vs = os.path.exists('.Senha')
+        if vs:
+            rsenha = open('.Senha', 'r')
+        print(menu_cores)
+        name = str(input(f'\n{am}//: {br}'))
+        os.chdir('/data/data/com.termux/files/usr/etc')
+        os.system('rm -rf bash.bashrc')
+        if name == '1':
+            snick = nick_banner.replace(':cor', '31')
+        elif name == '2':
+            snick = nick_banner.replace(':cor', '33')
+        elif name == '3':
+            snick = nick_banner.replace(':cor', '37')
+        elif name == '4':
+            snick = nick_banner.replace(':cor', '32')
+        elif name == '5':
+            snick = nick_banner.replace(':cor', '36')
+        elif name == '6':
+            snick = nick_banner.replace(':cor', '34')
+        elif name == '7':
+            snick = nick_banner.replace(':cor', '35')
+        else:
+            print(f'{ve}{cores} não é um comando')
+            sleep(2)
+            os.system('clear')
+            break
         nick_file = open('.Nick', 'w')
         nick_file.write(f'{snick}')
         nick_file.close()
@@ -45,5 +44,4 @@ def banner():
         bash.write(f'{inicio}\n' + f'{rsenha.read()}\n{rnick.read()}\n{rcor.read()}'if vs
                    else f'{rnick.read()}\n{rcor.read()}\n{final}')
         bash.close()
-        rnick.close()
-        os.system('clear')
+        break
